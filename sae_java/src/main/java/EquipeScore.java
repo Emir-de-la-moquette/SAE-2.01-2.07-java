a3c5d13256bd231613760729abefead3c0c9459e
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-04-08 11:41:07 +0200

    gestion tuple et base de match

---
+++ sae_java/src/main/java/EquipeScore.java
@@ -0,0 +1,8 @@
+public class EquipeScore<X, Y> { 
+    public final Equipe x; 
+    public final int y; 
+    public EquipeScore(Equipe x, int y) { 
+      this.x = x; 
+      this.y = y; 
+    } 
+  } 
--- sae_java/src/main/java/Match.java
+++ sae_java/src/main/java/Match.java
@@ -1,5 +1,8 @@
-
-
 public class Match {
-    
+    public Match(){
+    }
+
+    public EquipeScore<Equipe,Integer> deroulerMatchSolo(){
+        return new EquipeScore<>(null, 0);
+    }
 }
