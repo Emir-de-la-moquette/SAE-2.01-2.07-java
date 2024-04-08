28b5bfcb706899e235aeee7709fc40c29e67c351
BastienMonet <monet.bastien01@gmail.com>
2024-04-08 12:22:35 +0200

    début création lanceEpreuve

--- sae_java/src/main/java/1/Epreuve.java
+++ sae_java/src/main/java/1/Epreuve.java
@@ -7,17 +7,25 @@ public class Epreuve {
     private String sexeEpreuve;
     private String categorieEpreuve;
     private String typeEpreuve;
-    // private  scoreEquipes
+
+    private List<Pair<Equipe, Integer>> scoreDesEquipes;
 
     private List<Equipe> lesEquipes;
+    private Sport leSport;
 
     
-    
-    public Epreuve(String nomEpreuve, String sexeEpreuve, String categorieEpreuve, String typeEpreuve) {
+
+    public Epreuve(String nomEpreuve, String sexeEpreuve, String categorieEpreuve, String typeEpreuve, Sport sport) {
         this.nomEpreuve = nomEpreuve;
         this.sexeEpreuve = sexeEpreuve;
         this.categorieEpreuve = categorieEpreuve;
         this.typeEpreuve = typeEpreuve;
+
+        this.sport = sport;
+
+        this.scoreDesEquipes = new ArrayList<>();
+        this.lesEquipes = new ArrayList<>();
+
     }
 
     public String getNomEpreuve() {
@@ -51,5 +59,55 @@ public class Epreuve {
     public void setTypeEpreuve(String typeEpreuve) {
         this.typeEpreuve = typeEpreuve;
     }
+
+
+    public Sport getLesSports() {
+        return lesSports;
+    }
+
+    public void setLesSports(Sport lesSports) {
+        this.lesSports = lesSports;
+    }
     
+    // @param : une équipe
+    // fait participer une équipe à l'épreuve
+    public void participer(Equipe equipe) {
+        if (equipe.size() == leSport.getNomSport())
+            this.lesEquipes.add(equipe);
+
+    }
+
+    // @param : une équipe
+    // retire une équipe à l'épreuve
+    public void retirer(Equipe equipe) {
+        this.lesEquipes.remove(equipe);
+
+    }
+
+    // affiche les équipes participant à l'épreuve       
+    public List<Equipe> AfficheLesEquipes() {
+        return lesEquipes;
+    }
+
+
+    public void lanceEpreuve() {
+
+
+        typeEpreuve = getTypeEpreuve();
+
+        if(typeEpreuve == "Duel") {
+
+        }
+
+        if(typeEpreuve == "Score") {
+            
+        }
+
+
+    }
+
+    @Override
+    public String ToString() {
+        return getCategorieEpreuve() + getNomEpreuve() + getSexeEpreuve();
+    }
 }
