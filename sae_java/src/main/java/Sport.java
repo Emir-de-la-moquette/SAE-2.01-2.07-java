e794087a6b22b2eb7f1d4b32241c5926c53e0175
clement <143810240+colomonos@users.noreply.github.com>
2024-06-13 21:48:25 +0200

    docStringSport.java

--- sae_java/src/main/java/Sport.java
+++ sae_java/src/main/java/Sport.java
@@ -48,54 +48,107 @@ public class Sport {
         this.nbdePointmax = nbdePointmax;
     }
 
+    /*
+    *retourne le nombre de point max d'un sport
+    *@return int nbdePointmax
+    */
     public int getpoint() {
         return RandomNumberInRange.getRandomInt(1, this.nbdePointmax);
     }
 
+    /*
+    *retourne le nom du sport
+    *@return String nomSport
+    */
     public String getNomSport() {
         return nomSport;
     }
 
+    /*
+    *modifie le nom du sport
+    *@param String nomSport
+    */
     public void setNomSport(String nomSport) {
         this.nomSport = nomSport;
     }
 
+    /*
+    *retourne le nombre de joueur pour un sport
+    *@return int nbJoueur
+    */
     public int getNbJoueur() {
         return nbJoueur;
     }
 
+    /*
+    *modifie le nombre de joueur pour un sport
+    *@param int nbJoueur
+    */
     public void setNbJoueur(int nbJoueur) {
         this.nbJoueur = nbJoueur;
     }
 
+    /*
+    *retourne la valeur Agilite pour un sport
+    *@return Double valeurAgilite
+    */
     public Double getValeurAgilite() {
         return valeurAgilite;
     }
 
+     /*
+    *modifie la valeur Agilite pour un sport
+    *@param Double valeurAgilite
+    */
     public void setValeurAgilite(Double valeurAgilite) {
         this.valeurAgilite = valeurAgilite;
     }
 
+    /*
+    *retourne la valeur Endurance pour un sport
+    *@return Double valeurEndurance
+    */
     public Double getValeurEndurance() {
         return valeurEndurance;
     }
 
+    /*
+    *modifie la valeur Endurance pour un sport
+    *@param Double valeurEndurance
+    */
     public void setValeurEndurance(Double valeurEndurance) {
         this.valeurEndurance = valeurEndurance;
     }
 
+    /*
+    *retourne la valeur Force pour un sport
+    *@return Double valeurForce
+    */
     public Double getValeurForce() {
         return valeurForce;
     }
 
+    /*
+    *modifie la valeur Force pour un sport
+    *@param Double valeurForce
+    */
     public void setValeurForce(Double valeurForce) {
         this.valeurForce = valeurForce;
     }
 
+    /*
+    *retourne vrai ou faux si lees regles special son respecter pour un sport
+    *@return boolean hasregle
+    */
     public boolean hasReglePersonalisee() {
         return hasregle;
     }
 
+    /*
+    *retourne vrai ou faux pour la conditionVictoire si elle est respecter pour un sport
+    *@param Double a, Double b
+    *@return boolean si la condition de victoire est valider
+    */
     public boolean conditionVictoire(Double a, Double b) {
         if ((a >= nbPointVictoireTotale || b >= nbPointVictoireTotale) && nbPointVictoireTotale != null)
             return true;
@@ -108,6 +161,10 @@ public class Sport {
         return false;
     }
 
+    /*
+    *modifie les regle pour un sport
+    *@param int nbPointVictoireTotale int nbPointMiniPourVictoire int ecartDePointMini
+    */
     public void setRegle(int nbPointVictoireTotale, int nbPointMiniPourVictoire, int ecartDePointMini) {
         this.nbPointVictoireTotale = nbPointVictoireTotale;
         this.nbPointMiniPourVictoire = nbPointMiniPourVictoire;
