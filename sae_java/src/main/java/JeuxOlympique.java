35dd6a23f919f7b01d37b4cc630add0cef6b3569
clement <143810240+colomonos@users.noreply.github.com>
2024-06-13 19:51:27 +0200

    docString JeuxOlympique.java

--- sae_java/src/main/java/JeuxOlympique.java
+++ sae_java/src/main/java/JeuxOlympique.java
@@ -25,59 +25,106 @@ public class JeuxOlympique {
         this.lesSports = new ArrayList<>();
     }
 
+    /*
+    *retourne le Lieu des jo
+    *@return String lieu
+    */
     public String getLieu() {
         return lieu;
     }
 
+    /*
+    *modifie le Lieu des jo
+    *@param String lieu
+    */
     public void setLieu(String lieu) {
         this.lieu = lieu;
     }
 
+    /*
+    *retourne l'annee des jo
+    *@return int annee
+    */
     public int getAnnee() {
         return annee;
     }
 
+    /*
+    *modifie l'annee des jo
+    *@param int annee
+    */
     public void setAnnee(int annee) {
         this.annee = annee;
     }
 
+    /*
+    *ajoute un sport pour les jo
+    *@param  Sport sport
+    */
     public void ajouteSport(Sport sport) {
         this.lesSports.add(sport);
 
     }
 
+    /*
+    *ajoute une epreuve pour les jo
+    *@param  Epreuve epreuve
+    */
     public void ajouteEpreuve(Epreuve epreuve) {
         this.lesEpreuve.add(epreuve);
 
     }
 
+    /*
+    *retire un sport pour les jo
+    *@param  Sport sport
+    */
     public void retireSport(Sport sport) {
         this.lesSports.remove(sport);
 
     }
 
+    /*
+    *retire une epreuve pour les jo
+    *@param  Epreuve epreuve
+    */
     public void retireEpreuve(Epreuve epreuve) {
         this.lesEpreuve.remove(epreuve);
 
     }
 
+    /*
+    *ajoute un Pays pour les jo
+    *@param  Pays pays
+    */
     public void ajoutePays(Pays pays) {
         this.lesPays.add(pays);
 
     }
 
+    /*
+    *retire un Pays pour les jo
+    *@param  Pays pays
+    */
     public void retirePays(Pays pays) {
         this.lesPays.add(pays);
 
     }
 
+    
+    /*
+    *fonction executer toute les épreuve placée dans un jeuxOlympique
+    */
     public void simulJO() {
         for (Epreuve epreuve : this.lesEpreuve) {
             epreuve.lanceEpreuve();
         }
     }
 
-    /* @return une liste des pays */
+    /*
+    *elle classe les pays via un comparateur en fonction des medailles
+    *@return List<Pays> classement d'une liste des pays
+    */
     public List<Pays> classement(Comparator<Pays> compare) {
         Collections.sort(this.lesPays, compare);
         return this.lesPays;
