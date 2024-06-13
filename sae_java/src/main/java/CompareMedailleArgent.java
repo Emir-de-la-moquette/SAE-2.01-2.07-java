2a345aa2a739530ff68c48cad9954b2aa3d02e4b
clement <143810240+colomonos@users.noreply.github.com>
2024-06-13 18:30:50 +0200

    doctString CompareMedailleArgent.java

--- sae_java/src/main/java/CompareMedailleArgent.java
+++ sae_java/src/main/java/CompareMedailleArgent.java
@@ -2,6 +2,10 @@ import java.util.Comparator;
     
 public class CompareMedailleArgent implements Comparator<Pays>{
 
+    /*
+    *retourne la diffence entre deux pays en terme de medailles dargent 
+    *@return int (si positif p1 (pay1) est au dessus de p2 (pays2) sinon p1 est au dessous de p2 si p1 et p2 sont egale a eux meme representer par 0  on place par ordre d'arriver
+    */
     @Override
     public int compare(Pays p1, Pays p2){
         int m1 = p1.getmedailles_argent();
