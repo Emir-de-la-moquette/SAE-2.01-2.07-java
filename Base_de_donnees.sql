54160fbc9e843b09832f3c3035528fb3fc12594d
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-06-18 17:20:28 +0200

    BASE DE DONNEE BIEN FAITE

--- Base_de_donnees.sql
+++ Base_de_donnees.sql
@@ -14,73 +14,117 @@ drop table Sport;
 
 
 create table Sport(
-    id_sport varchar(70),
-    valeurForce INT(100),
-    valeurEndurance INT(100),
-    valeurAgilite INT(100),
+    nom_sport varchar(32),
     nbjoueur INT,
-     PRIMARY KEY (id_sport));
+     PRIMARY KEY (id_sport)
+);
 
 create table JeuxOlympique(
-    id_JeuxOlympique INT,
-    nomJeuxOlympique varchar(40),
-    lieu varchar(100),
-    annee DATE,
-    id_sport varchar(70),
-    PRIMARY KEY (id_JeuxOlympique));
+    nomJO varchar(100),
+    lieu varchar(64),
+    annee INT,
+    id_sport varchar(32),
+    PRIMARY KEY (nomJO)
+);
+
 
 create table Epreuve(
     id_Epreuve INT,
-    id_sport varchar(70),
-    nomEpreuve varchar(100),
+    nomEpreuve varchar(64),
     categorieEpreuve varchar(100),
-    typeEpreuve varchar(100),
-    sexEpreuve varchar(1),
-    PRIMARY KEY (id_Epreuve));
+    typeEpreuve varchar(10),
+    sexEpreuve char(1),
+    coefForce DOUBLE(1,3) unsigned,
+    coefEndurance DOUBLE(1,3) unsigned,
+    coefAgilite DOUBLE(1,3) unsigned,
+    PRIMARY KEY (id_Epreuve)
+);
 
 create table Matchs(
     id_Match INT,
     id_Epreuve INT,
-    Matchtypes varchar(10),
-    scoreE1 INT,
-    scoreE2 INT,
+    scoreE1 DOUBLE(3,10) unsigned,
+    scoreE2 DOUBLE(3,10) unsigned,
     equipe1 INT,
     equipe2 INT,
 
-    PRIMARY KEY (id_Match,Matchtypes));
+    PRIMARY KEY (id_Match)
+);
 
 create table Pays(
-    id_pays varchar(100),
+    nom_pays varchar(100),
     medpays_or INT,
     medpays_argent INT,
     medpay_bronze INT,
-    id_JeuxOlympique INT,
-    PRIMARY KEY (id_pays));
+    PRIMARY KEY (nom_pays)
+);
 
 create table Athlete(
     id_Athlete INT,
     NomAt varchar(50),
     PrenomAT varchar(50),
-    SexeAT varchar(1),
-    stats_Force INT(100),
-    stats_Endurance INT(100),
-    stats_agilite INT(100),
-    id_Equipe INT,
-    PRIMARY KEY (id_Athlete));
+    SexeAT char(1),
+    stats_Force INT,
+    stats_Endurance INT,
+    stats_agilite INT,
+    PRIMARY KEY (id_Athlete)
+);
 
 create table Equipe(
     id_Equipe INT,
+    taille_Equipe INT,
     medEquipe_or INT,
     medEquipe_argent INT,
     medEquipe_bronze INT,
-    sexeEquipe varchar(1),
-    PRIMARY KEY (id_Equipe));
+    sexeEquipe char(1),
+    PRIMARY KEY (id_Equipe)
+);
+
+create table ContenirEQ_ATH(
+    id_Athlete INT,
+    id_Equipe INT,
+    PRIMARY KEY (id_Athlete, id_Equipe)
+);
+
+create table ContenirJO_SP(
+    nomJO varchar(100),
+    nom_sport varchar(32),
+    PRIMARY KEY (nomJO, nom_sport)
+);
+
+create table ContenirJO_EP(
+    nomJO varchar(100),
+    id_Epreuve INT,
+    PRIMARY KEY (nomJO, id_Epreuve)
+);
+
+create table ContenirJO_PAYS(
+    nomJO varchar(100),
+    nom_pays varchar(100),
+    PRIMARY KEY (nomJO, nom_pays)
+);
+
+create table ContenirPAYS_EQ(
+    id_Equipe varchar(100),
+    nom_pays varchar(100),
+    PRIMARY KEY (id_Equipe, nom_pays)
+);
+
+create table ContenirEP_SP(
+    id_Epreuve INT,
+    nom_sport varchar(32),
+    PRIMARY KEY (id_Epreuve, nom_sport)
+);
+
 
 ALTER TABLE JeuxOlympique ADD FOREIGN KEY (id_sport) REFERENCES Sport(id_sport);
 ALTER TABLE Athlete ADD FOREIGN KEY (id_Equipe) REFERENCES Equipe(id_Equipe);
 ALTER TABLE Pays ADD FOREIGN KEY (id_JeuxOlympique) REFERENCES JeuxOlympique(id_JeuxOlympique);
 ALTER TABLE Epreuve ADD FOREIGN KEY (id_sport) REFERENCES Sport(id_sport);
 ALTER TABLE Matchs ADD FOREIGN KEY (id_Epreuve) REFERENCES Epreuve(id_Epreuve);
+ALTER TABLE Matchs ADD FOREIGN KEY (equipe1) REFERENCES Equipe(id_Equipe);
+ALTER TABLE Matchs ADD FOREIGN KEY (equipe2) REFERENCES Equipe(id_Equipe);
+
 
 
 --revoir pour match
