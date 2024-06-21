b0d8b3ebdc99356fb080f76db3487cdf4fb17634
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-06-21 10:07:30 +0200

    Update ConnexionMySQL.java

--- sae_java/src/main/java/ConnexionMySQL.java
+++ sae_java/src/main/java/ConnexionMySQL.java
@@ -1,36 +1,45 @@
+
+
 import java.sql.*;
 
-public class ConnexionMySQL {
+public class ConnexionMySql {
+    
 	private Connection mysql;
-	private boolean connecte=false;
-	public ConnexionMySQL() throws ClassNotFoundException{
-		this.mysql=null;
-		this.connecte=false;
-		Class.forName("org.mariadb.jdbc.Driver");
-	}
 
-	public void connecter() throws SQLException {
-		this.mysql=null;
-		this.connecte=false;
+	public ConnexionMySql() throws SQLException{
 		this.mysql = DriverManager.getConnection(
-					"jdbc:mysql://servinfo-maria:3306/DBdantec","dantec", "dantec");
-		this.connecte=true;
-	}
+					"jdbc:mysql://localhost:3307/saef","root","4dameorc");
+                }
+
+                
+	/*public ConnexionMySql(String leGens) throws SQLException{
+        this.mysql = DriverManager.getConnection(
+                     "jdbc:mysql://servinfo-maria:3306/DB"+leGens,leGens,leGens);
+                    //"jdbc:mysql://servinfo-maria:3306/DBmarques","marques","marques");
+					// "jdbc:mysql://"+nomServeur+":3306/"+nomBase,nomLogin, motDePasse);
+
+    } */
+
+
 	public void close() throws SQLException {
 		this.mysql.close();
-		this.connecte=false;
 	}
+    public Statement createStatement() throws SQLException {
+        if (this.mysql == null) {
+            throw new SQLException("pas connecté");
+        }
+        return this.mysql.createStatement();
+    }
 
-    public boolean isConnecte() { return this.connecte;}
-    public Blob createBlob()throws SQLException{
-		return this.mysql.createBlob();
-	}
-	public Statement createStatement() throws SQLException {
-		return this.mysql.createStatement();
-	}
 
 	public PreparedStatement prepareStatement(String requete) throws SQLException{
 		return this.mysql.prepareStatement(requete);
 	}
+
+
+    public void executeQuery(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'executeQuery'");
+    }
 	
 }
