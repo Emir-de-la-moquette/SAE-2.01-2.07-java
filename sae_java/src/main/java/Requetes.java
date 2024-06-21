b7e68a32b9a2e8adc80218705a611166664d6ed3
BastienMonet <monet.bastien01@gmail.com>
2024-06-21 11:19:04 +0200

    push direct dans le main on a pas le temps

--- sae_java/src/main/java/Requetes.java
+++ sae_java/src/main/java/Requetes.java
@@ -1,12 +1,11 @@
 import java.sql.*;
 
 public class Requetes {
-    private ConnexionMySQL connexion;
+    private ConnexionMySql connexion;
     private Statement st;
 
-    public Requetes() throws ClassNotFoundException, SQLException {
-        this.connexion = new ConnexionMySQL();
-        this.connexion.connecter();
+    public Requetes(ConnexionMySql connexion) throws ClassNotFoundException, SQLException {
+        this.connexion = new ConnexionMySql();
     }
 
     public void ajouterAthlete(Athlete athlete) throws SQLException {
@@ -406,43 +405,80 @@ public class Requetes {
         }
     }
 
-    public void newUser(User utilisateur) {
+    
         
-        supprimerUser(utilisateur.getMail());
-        
-        PreparedStatement ps = this.connexion.prepareStatement("insert into utilisateur(identifiantu, motdepasse, role_utilidsa) values (?, ?, ?)");
-        ps.setString(1, utilisateur.getMail());
-        ps.setString(2, utilisateur.getMdp());
-        ps.setString(3, ""+utilisateur.getRole());
+    public void ajouteAdmin() throws SQLException {
+
+
+        PreparedStatement ps = this.connexion.prepareStatement("insert into UTILISATEUR(identifiantu, motdepasse , role_utilidsa) values (?, ?, ?)");
+        ps.setString(1, "a");
+        ps.setString(2, "1234");
+        ps.setString(3, "A");
 
         ps.executeUpdate();
     }
 
-    public boolean mailExiste(User user) {
-        try {
-            String requete = "select * from UTILISATEUR where identifiantu =\"" + user.getMail();
-            ResultSet rs=st.executeQuery(requete);
-            return true;
-            } catch (SQLException e) { 
-                return false;
-            }
-    }
+    // public boolean UtilisateurExist(User user) throws SQLException {
+    //     try {
+    //     String requete = "select * from UTILISATEUR where identifiantu = \"" + user.getMail();
+    //     ResultSet rs=st.executeQuery(requete);
+    //     return false;
+    //     } catch (SQLException e) { 
+    //         return true;
+    //     }
+    // }
 
-    public char getRole(String mail) {
-        try {
-            String requete = "select * from UTILISATEUR where identifiantu =\"" + mail;
-            ResultSet rs=st.executeQuery(requete);
-            return rs.getString("role_utilidsa").toCharArray()[0];
-        }catch (SQLException e) { 
-            throw e;
-        }
-    }
+    
+    // public boolean LoginCorrect(User user) throws SQLException {
+    //     try {
+    //     String requete = "select * from UTILISATEUR where identifiantu =\"" + user.getMail() + "\"and motdepasse\"" + user.getMdp();
+    //     ResultSet rs=st.executeQuery(requete);
+    //     return true;
+    //     } catch (SQLException e) { 
+    //         return false;
+    //     }
+    // }
+    
 
-    public void editUser(User utilisateur, User editedUser) {
-        supprimerUser(editedUser.getMail());
-        newUser(utilisateur);
-    }
+}
+
+
+    // public void ajouterUser(User utilisateur) {
+        
+    //     supprimerUser(utilisateur.getMail());
+        
+    //     PreparedStatement ps = this.connexion.prepareStatement("insert into utilisateur(identifiantu, motdepasse, role_utilidsa) values (?, ?, ?)");
+    //     ps.setString(1, utilisateur.getMail());
+    //     ps.setString(2, utilisateur.getMdp());
+    //     ps.setString(3, ""+utilisateur.getRole());
+
+    //     ps.executeUpdate();
+    // }
+
+    // public boolean mailExiste(User user) {
+    //     try {
+    //         String requete = "select * from UTILISATEUR where identifiantu =\"" + user.getMail();
+    //         ResultSet rs=st.executeQuery(requete);
+    //         return true;
+    //         } catch (SQLException e) { 
+    //             return false;
+    //         }
+    // }
+
+    // public char getRole(String mail) {
+    //     try {
+    //         String requete = "select * from UTILISATEUR where identifiantu =\"" + mail;
+    //         ResultSet rs=st.executeQuery(requete);
+    //         return rs.getString("role_utilidsa").toCharArray()[0];
+    //     }catch (SQLException e) { 
+    //         throw e;
+    //     }
+    // }
+
+    // public void editUser(User utilisateur, User editedUser) {
+    //     supprimerUser(editedUser.getMail());
+    //     newUser(utilisateur);
+    // }
     
 
 
-}
